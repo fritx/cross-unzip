6e657ace2686e4e640251abcaf338e8f045e3397
Quatrecentquatre <obvious.ly.dev@gmail.com>
2021-05-30 15:25:41 +0200

    Update index.js
    Object deconstruction should be better to reduce lines.

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 'use strict'
-var spawn = require('child_process').spawn
-var relative = require('path').relative
-var basename = require('path').basename
-var dirname = require('path').dirname
-var slice = Array.prototype.slice
-var isWin = process.platform === 'win32'
+const {spawn} = require('child_process')
+const {relative, basename, dirname} = require('path')
+const {slice} = Array.prototype
+const _7z = require('win-7zip')['7z']
+const isWin = process.platform === 'win32'
 
 // todo: progress feedback
 
-function zip (src, pack, callback) {
+function zip(src, pack, callback) {
   if (isWin) {
-    var _7z = require('win-7zip')['7z']
     // eg. 7z a -tzip archive.zip ./archive
     run(_7z, ['a', '-tzip', pack, src], callback)
   } else {
     // inspired by cross-zip
     // https://github.com/feross/cross-zip/blob/abf9908a259988657c773ea111f83dfcece2ff5f/index.js#L80-L82
-    var dir = dirname(src)
+    var cwd = dirname(src)
     var file = basename(src)
     run('zip', ['-r', '-y', pack, file], {
-      cwd: dir
+      cwd
     }, callback)
   }
 }
 
-function unzip (pack, dest, callback) {
+function unzip(pack, dest, callback) {
   if (isWin) {
-    var _7z = require('win-7zip')['7z']
-    // 确实奇葩
     // eg. 7z x archive.zip -oc:\Doc
     run(_7z, ['x', pack, '-y', '-o' + dest], callback)
   } else {
@@ -39,7 +35,7 @@ function unzip (pack, dest, callback) {
 // Note that the 'exit' event may or may not fire after an error has occurred.
 // If you are listening to both the 'exit' and 'error' events,
 // it is important to guard against accidentally invoking handler functions multiple times.
-function run (bin, args, opts, callback) {
+function run(bin, args, opts, callback) {
   if (!callback) {
     callback = opts
     opts = null
@@ -61,7 +57,7 @@ function run (bin, args, opts, callback) {
 // http://stackoverflow.com/questions/30234908/javascript-v8-optimisation-and-leaking-arguments
 // javascript V8 optimisation and “leaking arguments”
 // making callback to be invoked only once
-function onceify (fn) {
+function onceify(fn) {
   var called = false
   return function () {
     if (called) return
@@ -70,8 +66,5 @@ function onceify (fn) {
   }
 }
 
-
-unzip.isWin = isWin
-unzip.unzip = unzip
-unzip.zip = zip
+unzip = {...unzip, isWin, unzip, zip}
 module.exports = unzip
